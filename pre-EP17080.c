29d21186edcfb86659d149eaca3a5de4b3b2d724
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 16:05:35 +0900

    gccss

--- pre-EP17080.c
+++ pre-EP17080.c
@@ -3,13 +3,12 @@ int main(void){
   long x,n,m,i,x1,x2;
   scanf("%ld %ld %ld",&x,&n,&m);
   x1=x;
-  x2=0;
+  x3=0;
   for(i=1;i<n;i++){
     x=x*x1;
     x2=x1%m;
-    x2+=;
+    x3=x3+x2;
   }
-  
-  printf("%ld\n",x2);
+  printf("%ld\n",x3);
   return 0;
 }
