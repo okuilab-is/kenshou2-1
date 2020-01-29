887f753110c8e5772005fd99c44533a100e17cfa
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 16:52:29 +0900

    gccss

--- pre-EP16026.c
+++ pre-EP16026.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 //aaaa
 int main(void){
-  long x,n,m,i,ans1=1,ans2,ans3=0;
+  long x,n,m,i,ans1=1;
   scanf("%ld",&x);
   scanf("%ld",&n);
   scanf("%ld",&m);
   for(i=1;i<=n;++i){
     ans1=ans1*x;
-    if(ans1>=m) ans2=ans1%m;
-    else ans2=ans1;
   }
-  printf("%ld\n",ans2);
+  ans1=ans1%m;
+  printf("%ld\n",ans1);
 }
