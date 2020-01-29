7b51b0898ad3a296ea397094c42c1f5d73a940a0
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 15:50:12 +0900

    gccss

---
+++ pre-ep17014.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (void){
+  long x,n,m,i,a=0,am=0,sum;
+  scanf("%ld %ld %ld",&x,&n,&m);
+  if(n==0){
+    sum=0;
+  }else{
+    sum=x;
+    for(i=0;i<n-1;i++){
+      sum=sum*x;
+      a=(sum-am)%m;
+      am+=a;
+    }
+  }
+  printf("%ld",a);
+}
