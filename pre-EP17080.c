14c2eb946c59da9d2257fd88ec591b1a098c3cfe
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 16:00:16 +0900

    gccss

--- pre-EP17080.c
+++ pre-EP17080.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
 int main(void){
   long x,n,m,i,x1,x2;
   scanf("%ld %ld %ld",&x,&n,&m);
   x1=x;
+  x2=0;
   for(i=1;i<n;i++){
     x=x*x1;
-    x2=x%m;
+    x2=x1%m;
+    x2+=;
   }
   
   printf("%ld\n",x2);
