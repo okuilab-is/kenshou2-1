aaa99f7b44e2717419438aeba59d0901e414a2c1
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 15:51:24 +0900

    gccss

--- pre-ep17063.c
+++ pre-ep17063.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-  long x,n,m,pow=1;//long int 型
+  long pow=1,x,n,m;//long int 型
   int i;
   scanf("%ld %ld %ld",&x,&n,&m);
   for(i=0;i<n;i++){
-    pow*=x;
+    pow=pow*x;
   }
   printf("%ld \n",pow/m);
 
