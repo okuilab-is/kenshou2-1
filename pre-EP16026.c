94bde97411ef0fb3813bb439d5b7fbd9b2a2e768
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 16:30:02 +0900

    gccss

--- pre-EP16026.c
+++ pre-EP16026.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 //aaaa
 int main(void){
-  long x,n,m,i,ans1,ans2;
+  long x,n,m,i,ans1,ans2,ans3=0;
   scanf("%ld",&x);
   scanf("%ld",&n);
   scanf("%ld",&m);
@@ -9,6 +9,8 @@ int main(void){
   for(i=0;i<=n;++i){
     ans1=ans1*x;
     ans2=ans1%m;
+    ans3=ans3+ans2;
   }
-  printf("%ld/n",ans2);
+  ans3=ans3%m;
+  printf("%ld\n",ans3);
 }
