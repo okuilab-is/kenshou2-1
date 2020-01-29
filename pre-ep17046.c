7a7160a7cc1fe392a1fcb69012811b50993ee4d0
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 15:55:13 +0900

    gccss

--- pre-ep17046.c
+++ pre-ep17046.c
@@ -5,9 +5,16 @@ int main(void){
  a=x%m;
  b=a;
  n-=1;
+ while(n>0){
+  if(n%2==0) b=(b*b)%m;
+  else b=(b*b*a)%m;
+  n=n/2;
+ }
+/*
  for(n;n>0;n--){
   b=(b*a)%m;
  }
+*/
  printf("%ld\n",b);
 }
 
