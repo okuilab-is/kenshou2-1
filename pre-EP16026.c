3ce1e0510309ea67d147d39b693e682cde83b502
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 16:12:48 +0900

    gccss

--- pre-EP16026.c
+++ pre-EP16026.c
@@ -2,10 +2,10 @@
 //aaaa
 int main(void){
   long x,n,m;
-  double i,ans;
+  int i,ans;
   scanf("%ld",&x);
   ans=x;
-  for(i=0.0,i<=n,++i){
+  for(i=0.0,i<=n,++i;){
     ans=ans*x;
   }
   ans=ans%m;
