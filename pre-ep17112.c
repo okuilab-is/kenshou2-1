ead75a49dec0872a0c0e201c9d0460dd28d09c35
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 15:55:41 +0900

    gccss

--- pre-ep17112.c
+++ pre-ep17112.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(void){
-		  long int x,m,n,i,ans=1;
+		  long x,m,n,i,ans=1;
 		  scanf("%ld%ld%ld", &x, &n, &m);
         for(i=0;i++;i<n){
 					 ans=ans*x;
-					 if(ans>m) ans=ans%m;
+					 ans=ans%m;
 		  }		 
 		  printf("%ld\n", ans);
 		  return 0;
