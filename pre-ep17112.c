67dd0413b08fc2d4cca6018a930b9a63990576e3
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 15:42:41 +0900

    gccss

--- pre-ep17112.c
+++ pre-ep17112.c
@@ -3,10 +3,6 @@
 int main(){
 		  long int x,m,n,ans,i;
 		  scanf("%ld%ld%ld", &x, &n, &m);
-		  ans=1;
-		  for(i=0;i++;i<n){
-					 ans=ans*x;
-		  }
-		  ans=ans%m;
-		  printf("%ld\n", ans);
+        ;
+		  printf("%ld%ld%ld\n", x, n, m);
 }
