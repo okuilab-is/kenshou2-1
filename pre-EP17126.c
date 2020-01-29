0eef972e8518d8a3ebf28992869f1824be55711c
ep <ep16018@sti.chubu.ac.jp>
2020-01-29 15:34:54 +0900

    gccss

--- pre-EP17126.c
+++ pre-EP17126.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 int main(void){
-	long x,n,m;
+	long x,n,m,i,a=1;
 	scanf("%ld %ld %ld",&x,&n,&m);
-	printf("%ld",(power(x,n)%m));
+	for(i=0;i<n;i++){
+		a*=x;
+	}
+	printf("%ld",a%m);
 }
